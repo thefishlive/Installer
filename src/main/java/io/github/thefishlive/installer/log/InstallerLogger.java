bfdec77ceb07dd791619d086d42b9ee7ca4e2a1a
James Fitzpatrick <thefishlive@hotmail.com>
2014-03-18 18:00:02 +0000

    Convert to log4j2

--- src/main/java/io/github/thefishlive/installer/log/InstallerLogger.java
+++ src/main/java/io/github/thefishlive/installer/log/InstallerLogger.java
@@ -1,23 +1,21 @@
 package io.github.thefishlive.installer.log;
 
-import io.github.thefishlive.installer.InstallerUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.message.Message;
+import org.apache.logging.log4j.spi.AbstractLogger;
+
 import lombok.Getter;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+@SuppressWarnings("serial")
+public class InstallerLogger extends AbstractLogger {
 
-public class InstallerLogger extends Logger {
-
-	@Getter private static final Logger infolog = Logger.getLogger("installer");
-	@Getter private static final Logger userlog = Logger.getLogger("installer.user");
-	@Getter private static final Logger debuglog = Logger.getLogger("installer.debug");
-	@Getter private static final Logger errorlog = Logger.getLogger("error");
-	
-	static {
-		if (InstallerUtils.isDebug()) {
-			debuglog.setLevel(Level.INFO);
-		}
-	}
+	@Getter private static final Logger infolog = LogManager.getLogger("installer");
+	@Getter private static final Logger userlog = LogManager.getLogger("installer.user");
+	@Getter private static final Logger debuglog = LogManager.getLogger("installer.debug");
+	@Getter private static final Logger errorlog = LogManager.getLogger("error");
 	
 	@Getter private static final InstallerLogger log = new InstallerLogger("installer");
 	
@@ -81,6 +79,54 @@ public class InstallerLogger extends Logger {
 	@Override
 	public void fatal(Object message, Throwable t) {
 		errorlog.fatal(message, t);
+	}
+
+	@Override
+	protected boolean isEnabled(Level level, Marker marker, Message data, Throwable t) {
+		return isEnabled0(level);
+	}
+
+	@Override
+	protected boolean isEnabled(Level level, Marker marker, Object data, Throwable t) {
+		return isEnabled0(level);
+	}
+
+	@Override
+	protected boolean isEnabled(Level level, Marker marker, String data) {
+		return isEnabled0(level);
+	}
+
+	@Override
+	protected boolean isEnabled(Level level, Marker marker, String data, Object... p1) {
+		return isEnabled0(level);
+	}
+
+	@Override
+	protected boolean isEnabled(Level level, Marker marker, String data, Throwable t) {
+		return isEnabled0(level);
+	}
+	
+	private boolean isEnabled0(Level level) {
+		if (level.intLevel() <= Level.WARN.intLevel()) {
+			return errorlog.isEnabled(level);
+		} else if (level.intLevel() <= Level.INFO.intLevel()) {
+			return infolog.isEnabled(level);
+		} else if (level.intLevel() <= Level.TRACE.intLevel()) {
+			return debuglog.isEnabled(level);
+		}
+		
+		return true;
+	}
+
+	@Override
+	public void log(Marker marker, String fqcn, Level level, Message data, Throwable t) {
+		if (level.intLevel() == Level.DEBUG.intLevel() || level.intLevel() == Level.TRACE.intLevel()) {
+			debuglog.log(level, marker, data, t);
+		} else if (level.intLevel() == Level.INFO.intLevel()) {
+			infolog.log(level, marker, data, t);
+		} else if (level.intLevel() == Level.WARN.intLevel() || level.intLevel() == Level.ERROR.intLevel() || level.intLevel() == Level.FATAL.intLevel()) {
+			errorlog.log(level, marker, data, t);
+		} 
 	}
 
 }
--- src/main/java/io/github/thefishlive/installer/log/MessageBoxAppender.java
+++ src/main/java/io/github/thefishlive/installer/log/MessageBoxAppender.java
@@ -1,26 +1,30 @@
 package io.github.thefishlive.installer.log;
 
+import java.io.Serializable;
+
 import javax.swing.JOptionPane;
 
-import org.apache.log4j.AppenderSkeleton;
-import org.apache.log4j.Level;
-import org.apache.log4j.spi.LoggingEvent;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 
-public class MessageBoxAppender extends AppenderSkeleton {
+@Plugin(name = "MessageBox", category = "Core", elementType = "appender", printObject = true)
+public class MessageBoxAppender extends AbstractAppender {
 
-	@Override
-	public void close() {
+	protected MessageBoxAppender(String name, Filter filter, Layout<? extends Serializable> layout) {
+		super(name, filter, layout);
 	}
 
 	@Override
-	public boolean requiresLayout() {
-		return true;
-	}
-
-	@Override
-	protected void append(LoggingEvent event) {
+	public void append(LogEvent event) {
 		int type = JOptionPane.PLAIN_MESSAGE;
-		
+
 		if (event.getLevel() == Level.WARN) {
 			type = JOptionPane.WARNING_MESSAGE;
 		} else if (event.getLevel() == Level.ERROR || event.getLevel() == Level.FATAL) {
@@ -28,8 +32,25 @@ public class MessageBoxAppender extends AppenderSkeleton {
 		} else if (event.getLevel() == Level.INFO || event.getLevel() == Level.DEBUG) {
 			type = JOptionPane.INFORMATION_MESSAGE;
 		}
-		
-		JOptionPane.showMessageDialog(null, layout.format(event), "Installer", type);
+
+		JOptionPane.showMessageDialog(null, getLayout().toSerializable(event), "Installer", type);
 	}
 
+	@PluginFactory
+	public static MessageBoxAppender createAppender(@PluginAttribute("name") String name, 
+														@PluginElement("Layout") Layout<? extends Serializable> layout, 
+														@PluginElement("Filters") Filter filter) {
+
+		if (name == null) {
+			LOGGER.error("No name provided for MessageBoxAppender");
+			return null;
+		}
+		
+		if (layout == null) {
+			LOGGER.error("No layout provided for MessageBoxAppender");
+			return null;
+		}
+		
+		return new MessageBoxAppender(name, filter, layout);
+	}
 }
